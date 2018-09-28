b7f6685eea7abc5ed48cf44842ccc425c893bc74
DaRekaze <darekaze@gmail.com>
2018-09-28 22:45:16 +0800

    modify folder structure

--- src/main.js
+++ src/main.js
@@ -1,19 +1,12 @@
 import Vue from "vue";
-import "./plugins/axios";
 import App from "./App.vue";
 import router from "./routes/router";
-import store from "./store";
-import "./registerServiceWorker";
+import store from "./stores/store";
+import "./plugins/axios";
+import "./plugins/registerServiceWorker";
+import "./plugins/vmd";
 
 Vue.config.productionTip = false;
-
-import { MdButton, MdContent, MdTabs } from "vue-material/dist/components";
-import "vue-material/dist/vue-material.min.css";
-import "vue-material/dist/theme/default.css";
-
-Vue.use(MdButton);
-Vue.use(MdContent);
-Vue.use(MdTabs);
 
 new Vue({
   router,
---
+++ src/plugins/vmd.js
@@ -0,0 +1,8 @@
+import Vue from "vue";
+import { MdButton, MdContent, MdTabs } from "vue-material/dist/components";
+import "vue-material/dist/vue-material.min.css";
+import "vue-material/dist/theme/default.css";
+
+Vue.use(MdButton);
+Vue.use(MdContent);
+Vue.use(MdTabs);
