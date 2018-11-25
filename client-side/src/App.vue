f9839956b7af919a1daf657863d85f7fce5af3d8
DaRekaze <darekaze@gmail.com>
2018-11-25 18:24:34 +0800

    Add router transition animation

--- client-side/src/App.vue
+++ client-side/src/App.vue
@@ -2,7 +2,14 @@
 <v-app>
   <t-header />
   <v-content>
-    <router-view />
+    <transition
+      name="fade"
+      mode="out-in"
+      @beforeLeave="beforeLeave"
+      @enter="enter"
+      @afterEnter="afterEnter">
+      <router-view />
+    </transition>
   </v-content>
   <t-footer />
 </v-app>
@@ -20,3 +27,17 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition-duration: 0.3s;
+  transition-property: opacity;
+  transition-timing-function: ease;
+}
+
+.fade-enter,
+.fade-leave-active {
+  opacity: 0
+}
+</style>
