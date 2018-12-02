f79d477d1c4e0038d2ff798074de415efbf8e21b
DaRekaze <darekaze@gmail.com>
2018-12-03 00:31:39 +0800

    Apply changes and re-deploy!!

---
+++ precache-manifest.52919c284f71a39a4bfa838ce7496d49.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "a124b8c51335a3fd58a8",
+    "url": "/audifactory/js/forget.b79529db.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/audifactory/robots.txt"
+  },
+  {
+    "revision": "a69a30ee8ea6c7b475e3",
+    "url": "/audifactory/css/loves.05310ba3.css"
+  },
+  {
+    "revision": "aa9a2341c57486cc36fe",
+    "url": "/audifactory/js/albums.5e6b1653.js"
+  },
+  {
+    "revision": "1cfcecbe1c55699b7818",
+    "url": "/audifactory/js/userprofile.9ebe2c8d.js"
+  },
+  {
+    "revision": "39bbd8311c913b95f953",
+    "url": "/audifactory/js/app.2d6d6eb5.js"
+  },
+  {
+    "revision": "f499fd21157898d3389f",
+    "url": "/audifactory/js/single-album.28535984.js"
+  },
+  {
+    "revision": "9be968145184698a5be3",
+    "url": "/audifactory/js/cart.24d76d45.js"
+  },
+  {
+    "revision": "07c32d6b8ea83ba153be",
+    "url": "/audifactory/js/purchase-history.3633f8b2.js"
+  },
+  {
+    "revision": "e6390df4998dd3398b33",
+    "url": "/audifactory/js/chunk-vendors.b6087483.js"
+  },
+  {
+    "revision": "a69a30ee8ea6c7b475e3",
+    "url": "/audifactory/js/loves.6a5a02d6.js"
+  },
+  {
+    "revision": "f499fd21157898d3389f",
+    "url": "/audifactory/css/single-album.59904791.css"
+  },
+  {
+    "revision": "b6731d5a4a5b0d39e855",
+    "url": "/audifactory/js/about.5b25a63e.js"
+  },
+  {
+    "revision": "f06e25351a43ebc4a386",
+    "url": "/audifactory/js/albums-mod.e69601d8.js"
+  },
+  {
+    "revision": "ab976a543d8b74550f8259777c2fd655",
+    "url": "/audifactory/index.html"
+  },
+  {
+    "revision": "19e932264f8e6cb7e19094fd2f938dd2",
+    "url": "/audifactory/img/home.19e93226.jpg"
+  },
+  {
+    "revision": "07c32d6b8ea83ba153be",
+    "url": "/audifactory/css/purchase-history.67780aa1.css"
+  },
+  {
+    "revision": "a124b8c51335a3fd58a8",
+    "url": "/audifactory/css/forget.0e433876.css"
+  },
+  {
+    "revision": "e6390df4998dd3398b33",
+    "url": "/audifactory/css/chunk-vendors.c2baf687.css"
+  },
+  {
+    "revision": "9be968145184698a5be3",
+    "url": "/audifactory/css/cart.111d398c.css"
+  },
+  {
+    "revision": "39bbd8311c913b95f953",
+    "url": "/audifactory/css/app.4b90ac9e.css"
+  },
+  {
+    "revision": "aa9a2341c57486cc36fe",
+    "url": "/audifactory/css/albums.5eec44e3.css"
+  }
+];
